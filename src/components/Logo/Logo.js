11922be2b3982939fb988623c5d7e0e96422a1df
Julia Tran <julia.kim.tran@hotmail.com>
2022-11-15 10:09:42 -0500

    Comments

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
 import config from '../../config';
-import MobileLogoImage from './cottagedays-logo-small.png';
-import DesktopLogoImage from './cottagedays-logo.png';
+import MobileLogoImage from './logo-small.png';
+import DesktopLogoImage from './logo-beta.png';
 import css from './Logo.module.css';
 
 const Logo = props => {
